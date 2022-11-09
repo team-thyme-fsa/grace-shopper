81beee1586fab2e81c9fe926754df7c07cad8902
fguillen1215 <111141844+fguillen1215@users.noreply.github.com>
2022-11-09 15:35:52 -0500

    created Order_Products model, changed Order.js file name to Orders.js

---
+++ server/db/models/OrderProducts.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Order_Products = db.define('order_products', {
+  quantity: {
+    type: Sequelize.INTEGER
+    defaultValue: 1
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    validate: {
+      min: 0
+    }
+  }
+});
+
+module.exports = Order_Products;
