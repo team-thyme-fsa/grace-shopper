c96db242d3ef8d9ffb11934f0c05319e5f64b83f
Elijah Rhinehardt <elijah.rhinehardt7@gmail.com>
2022-11-09 12:22:37 -0500

    Products model completed

--- server/db/models/Products.js
+++ server/db/models/Products.js
@@ -1,6 +1,33 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Product = db.define('product', {});
+const Product = db.define('product', {
+  name: {
+    type: Sequelize.STRING,
+    unique: true,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  price: {
+    type: Sequelize.INTEGER,
+    validate: {
+      min: 0,
+    },
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    default:
+      'https://upload.wikimedia.org/wikipedia/commons/5/53/Pok%C3%A9_Ball_icon.svg',
+  },
+});
 
 module.exports = Product;
