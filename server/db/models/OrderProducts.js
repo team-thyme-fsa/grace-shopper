5430bfc8d23e3ac2a796a9891b273b90af7bb534
Elijah Rhinehardt <elijah.rhinehardt7@gmail.com>
2022-11-09 16:55:14 -0500

    Fixed comma error

--- server/db/models/OrderProducts.js
+++ server/db/models/OrderProducts.js
@@ -3,15 +3,15 @@ const db = require('../db');
 
 const Order_Products = db.define('order_products', {
   quantity: {
-    type: Sequelize.INTEGER
-    defaultValue: 1
+    type: Sequelize.INTEGER,
+    defaultValue: 1,
   },
   price: {
     type: Sequelize.INTEGER,
     validate: {
-      min: 0
-    }
-  }
+      min: 0,
+    },
+  },
 });
 
 module.exports = Order_Products;
