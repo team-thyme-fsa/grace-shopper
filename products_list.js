5298ce87187fb32f01788fd407cc9d8cbb89674c
Elijah Rhinehardt <elijah.rhinehardt7@gmail.com>
2022-11-09 12:56:00 -0500

    Added pokeballs to the product list

--- products_list.js
+++ products_list.js
@@ -1,1 +1,100 @@
-export const products_list = [];
+export const products_list = [
+  {
+    name: 'Poké Ball',
+    type: 'POKEBALL',
+    description:
+      'A device for catching wild Pokémon. It is thrown like a ball at the target. It is designed as a capsule system.',
+    price: 200,
+    imageUrl:
+      'https://archives.bulbagarden.net/media/upload/7/79/Dream_Pok%C3%A9_Ball_Sprite.png',
+  },
+  {
+    name: 'Great Ball',
+    type: 'POKEBALL',
+    description:
+      'A good, high-performance Ball that provides a higher Pokémon catch rate than a standard Poké Ball.',
+    price: 600,
+    imageUrl:
+      'https://archives.bulbagarden.net/media/upload/b/bf/Dream_Great_Ball_Sprite.png',
+  },
+  {
+    name: 'Ultra Ball',
+    type: 'POKEBALL',
+    description:
+      'An ultra-performance Ball that provides a higher Pokémon catch rate than a Great Ball.',
+    price: 1200,
+    imageUrl:
+      'https://archives.bulbagarden.net/media/upload/a/a8/Dream_Ultra_Ball_Sprite.png',
+  },
+  {
+    name: 'Premier Ball',
+    type: 'POKEBALL',
+    description:
+      'A somewhat rare Poké Ball that has been specially made to commemorate an event of some sort.',
+    price: 350,
+    imageUrl:
+      'https://archives.bulbagarden.net/media/upload/6/64/Dream_Premier_Ball_Sprite.png',
+  },
+  {
+    name: 'Luxury Ball',
+    type: 'POKEBALL',
+    description:
+      'A comfortable Poké Ball that makes a caught wild Pokémon quickly grow friendly.',
+    price: 3000,
+    imageUrl:
+      'https://archives.bulbagarden.net/media/upload/7/7e/Dream_Luxury_Ball_Sprite.png',
+  },
+  {
+    name: 'Net Ball',
+    type: 'POKEBALL',
+    description:
+      'A somewhat different Poké Ball that works especially well on Water- and Bug-type Pokémon.',
+    price: 1000,
+    imageUrl:
+      'https://archives.bulbagarden.net/media/upload/a/a0/Dream_Net_Ball_Sprite.png',
+  },
+  {
+    name: 'Dive Ball',
+    type: 'POKEBALL',
+    description:
+      'A somewhat different Poké Ball that works especially well on Pokémon that live underwater.',
+    price: 1000,
+    imageUrl:
+      'https://archives.bulbagarden.net/media/upload/9/9a/Dream_Dive_Ball_Sprite.png',
+  },
+  {
+    name: 'Master Ball',
+    type: 'POKEBALL',
+    description:
+      'The best Ball with the ultimate level of performance. It will catch any wild Pokémon without fail.',
+    price: 1000000000000000,
+    imageUrl:
+      'https://archives.bulbagarden.net/media/upload/9/95/Dream_Master_Ball_Sprite.png',
+  },
+  {
+    name: 'Dusk Ball',
+    type: 'POKEBALL',
+    description:
+      'A somewhat different Poké Ball that makes it easier to catch wild Pokémon at night or in dark places like caves.',
+    price: 740,
+    imageUrl:
+      'https://archives.bulbagarden.net/media/upload/5/59/Dream_Dusk_Ball_Sprite.png',
+  },
+  {
+    name: 'Fast Ball',
+    type: 'POKEBALL',
+    description:
+      'A Poké Ball that makes it easier to catch Pokémon which are quick to run away.',
+    price: 810,
+    imageUrl:
+      'https://archives.bulbagarden.net/media/upload/4/44/Dream_Fast_Ball_Sprite.png',
+  },
+];
+
+// {
+//   name: '',
+//   type: '',
+//   description: '',
+//   price: ,
+//   imageUrl: '',
+// },
