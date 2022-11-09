ad353671bdab07ebfe62b68406b35ee8fcac5a14
ggiggity <nobodiesxp@gmail.com>
2022-11-09 15:29:16 -0500

    user seed data

---
+++ script/user.js
@@ -0,0 +1,46 @@
+const users = [
+  {
+    admin: false,
+    firstName: 'Peter',
+    lastName: 'Griffin',
+    email: 'birdistheword@mail.com',
+    password: 1232123,
+    address: 'Quahog Springfield 45st',
+  },
+  {
+    admin: false,
+    firstName: 'Joe',
+    lastName: 'Swanson',
+    email: 'iwannawalk@mail.com',
+    password: 9878987,
+    address: 'Quahog Springfield 40st',
+  },
+  {
+    admin: false,
+    firstName: 'Bob',
+    lastName: 'Burger',
+    email: 'cheeseburger@mail.com',
+    password: 4565456,
+    address: "Seymour's Bay North Jersey Shore, New Jersey",
+  },
+  {
+    admin: true,
+    firstName: 'Admin ',
+    lastName: 'master',
+    email: 'iamadmin@mail.com',
+    password: 001010,
+    address: 'secret',
+    orderId: 1,
+  },
+  {
+    admin: false,
+    firstName: 'Iron',
+    lastName: 'tony',
+    email: 'tonystark@mail.com',
+    password: 123123,
+    address: 'somewhere in there',
+    orderId: 2,
+  },
+];
+
+module.exports = users;
