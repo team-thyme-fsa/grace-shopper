79a357180b25b792cf4632b302fe66c81b42a402
fguillen1215 <111141844+fguillen1215@users.noreply.github.com>
2022-11-09 15:39:06 -0500

    set many to many associaton between Order and Product

--- server/db/index.js
+++ server/db/index.js
@@ -2,17 +2,24 @@
 
 const db = require('./db')
 
-const User = require('./models/User')
-const Order = require('./models/Order')
+const User = require('./models/Users')
+const Order = require('./models/Orders')
+const Product = require('./models/Products')
+const Order_Products = require('./models/OrderProducts')
 
 //associations could go here!
 Order.belongsTo(User);
 User.hasMany(Order);
 
+Order.belongsToMany(Product, { through: Order_Products });
+Product.belongsToMany(Order, { through: Order_Products });
+
 module.exports = {
   db,
   models: {
     User,
-    Order
+    Order,
+    Product,
+    Order_Products
   },
 }
