713e7c0b4f745cb3add684f776d625da77d5db67
fguillen1215 <111141844+fguillen1215@users.noreply.github.com>
2022-11-10 11:26:34 -0500

    created a single product route

--- server/api/products.js
+++ server/api/products.js
@@ -14,4 +14,13 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+// GET /api/products/id
+router.get('/:id', async (req, res, next) => {
+  try {
+    res.send(await Product.findByPk(req.params.id));
+  } catch (err) {
+    next(err);
+  }
+})
+
 module.exports = router;
