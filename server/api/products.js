c5a3038397cfa57ed251d5803ac384e2341d8f38
Elijah Rhinehardt <elijah.rhinehardt7@gmail.com>
2022-11-10 11:05:31 -0500

    All products route working

--- server/api/products.js
+++ server/api/products.js
@@ -1,6 +1,17 @@
 const express = require('express');
-const { Product } = require('../db');
+const {
+  models: { Product },
+} = require('../db');
 
 const router = express.Router();
+
+// GET /api/products
+router.get('/', async (req, res, next) => {
+  try {
+    res.send(await Product.findAll());
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
