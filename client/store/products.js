7aecf93f54f029e2ced75c4c8b873252d9644ce3
fguillen1215 <111141844+fguillen1215@users.noreply.github.com>
2022-11-11 11:29:03 -0500

    created single product thunk

--- client/store/products.js
+++ client/store/products.js
@@ -5,21 +5,30 @@ import history from '../history';
  * Action Types
  */
 const SET_ALL_PROD = 'SET_ALL_PROD';
+const SET_PROD = 'SET_PROD';
 
 /**
  * Action Creators
  */
 const setAllProd = (products) => ({ type: SET_ALL_PROD, products });
+const setProd = (product) => ({ type: SET_PROD, product})
 
 /**
  * Thunk Creators
  */
 export const fetchAllProd = () => {
   return async (dispatch) => {
-    const { data } = axios.get('/api/projects');
+    const { data } = axios.get('/api/products');
     dispatch(setAllProd(data));
   };
 };
+
+export const fetchProd = (id) => {
+  return async (dispatch) => {;
+    const { data } = axios.get(`/api/products/${id}`);
+    dispatch(setProd(data));
+  }
+}
 
 /**
  * Initial State
@@ -33,6 +42,9 @@ export default function productsReducer(state = initialState, action) {
   switch (action.type) {
     case SET_ALL_PROD: {
       return { ...state, products: [...action.products] };
+    }
+    case SET_PROD: {
+      return { ...state, product: {...action.product} };
     }
     default: {
       return state;
