92bf2ae3ae8d6a50dc09bea68664338c037b0ea1
Carina Belino <cbelino18@gmail.com>
2022-11-11 15:00:17 -0500

    added componentDidMount

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -3,3 +3,10 @@ import { connect } from 'react-redux';
 import { fetchAllProd } from '../store/products';
 
 import Product from './Product';
+
+// TODO(Carina): change to functional component
+export class AllProducts extends React.Component {
+  componentDidMount() {
+    this.props.fetchAllProd();
+  }
+}
