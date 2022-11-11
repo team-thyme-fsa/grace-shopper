f554c23687ad93ea52c9c7d37543665781fa0d41
Elijah Rhinehardt <elijah.rhinehardt7@gmail.com>
2022-11-11 11:38:11 -0500

    Created order.js reducer

---
+++ client/store/order.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import history from '../history';
+
+/**
+ * Action Types
+ */
+const CREATE_ORDER = 'CREATE_ORDER';
+
+/**
+ * Action Creators
+ */
+
+/**
+ * Thunk Creators
+ */
+
+/**
+ * Initial State
+ */
+
+export default function orderReducer(state = {}, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
