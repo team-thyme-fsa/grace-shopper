0edbffa40964171d0b72c3f5bae04cb2f453e889
Carina Belino <cbelino18@gmail.com>
2022-11-13 00:29:14 -0500

    added spacing in product widget

--- client/components/Product.js
+++ client/components/Product.js
@@ -8,13 +8,20 @@ const Product = (props) => {
       <img src={imageUrl} className="image" />
       <div className="product-info">
         <h3>{name}</h3>
-        <p>{price}</p>
-        {/* TODO(Carina): check button type="button" OR "submit" */}
-        <button type="button">ADD TO CART</button>
-        {/* TODO(Carina): add onClick */}
-        <button type="button" name="details">
-          DETAILS
-        </button>
+        <img src="Pokémon Dollar.png" alt="Pokémon dollar sign" width="12px" />
+        <p className="price">{price}</p>
+
+        <div className="buttons">
+          {/* TODO(Carina): check button type="button" OR "submit" */}
+          <button type="button">ADD TO CART</button>
+
+          <div id="details-button">
+            {/* TODO(Carina): add onClick */}
+            <button type="button" name="details">
+              DETAILS
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
