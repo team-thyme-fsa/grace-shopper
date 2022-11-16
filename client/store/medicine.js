dc54c83ac1e1dc35d9cde5c44335c0b42826dc0d
Carina Belino <cbelino18@gmail.com>
2022-11-16 08:45:33 -0500

    added action creator

--- client/store/medicine.js
+++ client/store/medicine.js
@@ -4,3 +4,8 @@ import axios from 'axios';
  * Action Type
  */
 const SET_ALL_MED = 'SET_ALL_MED';
+
+/*
+ * Action Creator
+ */
+const setAllMed = (medicines) => ({ type: SET_ALL_MED, medicines });
