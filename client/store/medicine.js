8fdb3beb51cedbd2ce87fdbf9b27d1e37497d062
Carina Belino <cbelino18@gmail.com>
2022-11-16 08:58:50 -0500

    added thunk creator

--- client/store/medicine.js
+++ client/store/medicine.js
@@ -9,3 +9,13 @@ const SET_ALL_MED = 'SET_ALL_MED';
  * Action Creator
  */
 const setAllMed = (medicines) => ({ type: SET_ALL_MED, medicines });
+
+/*
+ * Thunk Creator
+ */
+export const fetchAllMed = () => {
+  return async (dispatch) => {
+    const { data } = await axios.get('/api/medicine');
+    dispatch(setAllMed(data));
+  };
+};
