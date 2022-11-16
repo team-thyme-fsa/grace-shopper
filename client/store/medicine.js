704fd23a709d5ba8efd6cb7f33b027c47a437a84
Carina Belino <cbelino18@gmail.com>
2022-11-16 09:06:30 -0500

    added initial state and reducer

--- client/store/medicine.js
+++ client/store/medicine.js
@@ -19,3 +19,21 @@ export const fetchAllMed = () => {
     dispatch(setAllMed(data));
   };
 };
+
+/*
+ * Initial State
+ */
+const initialState = {
+  medicines: [],
+};
+
+export default function medicinesReducer(state = initialState, action) {
+  switch (action.type) {
+    case SET_ALL_MED: {
+      return { ...state, medicines: [...action.medicines] };
+    }
+    default: {
+      return state;
+    }
+  }
+}
